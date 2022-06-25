a247dc8f54f99388eefec4ea5d628767a782078a
Guadalupe1108 <mgaguilarl@unah.hn>
2022-06-25 00:20:00 -0600

    Proyecto1- vídeojuego/juegos

---
+++ EjerciciosDevC++/Ejercicios I Unidad/Proyecto1- vídeojuego/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "startShip.h"
+#include "snake.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    system("cls");
+    int opcion;
+
+    cout<<endl;
+    printf(" \
+	     ##     ## #### ########  ########  #######      ######      ###    ##     ## ########  ######  \n \
+            ##     ##  ##  ##     ## ##       ##     ##    ##    ##    ## ##   ###   ### ##       ##    ## \n \
+            ##     ##  ##  ##     ## ##       ##     ##    ##         ##   ##  #### #### ##       ##        \n \
+            ##     ##  ##  ##     ## ######   ##     ##    ##   #### ##     ## ## ### ## ######    ######    \n \
+             ##   ##   ##  ##     ## ##       ##     ##    ##    ##  ######### ##     ## ##             ##    \n \
+              ## ##    ##  ##     ## ##       ##     ##    ##    ##  ##     ## ##     ## ##       ##    ##     \n \
+               ###    #### ########  ########  #######      ######   ##     ## ##     ## ########  ######       \n");
+                  
+    
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"---- Seleccione un Juego ----"<<endl;
+    cout<<"--> 1. StarShip"<<endl;
+    cout<<"--> 2. Snake"<<endl;
+
+    cout<<endl;
+    cout<<"Ingrese un numero del menu para seleccionar un juego: " ; 
+    cin>>opcion;
+    switch (opcion)
+    {
+        
+    case 1: 
+            system("cls");
+            starShip();
+        break;
+    
+    case 2:
+            system("cls");
+            snake();
+        break;
+ 
+    default: 
+            cout<<"¡ Suerte para la proxima ! ";
+        break;
+
+    }
+
+    return 0;
+}
